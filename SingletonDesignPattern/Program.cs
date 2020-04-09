40cb29b1bcb6b9b703a8a83661afae35d90350de
PanKunik <kunik12254@o2.pl>
2020-04-09 17:14:03 +0200

    Adding Singleton design pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            if (s1 == s2)
+                Console.WriteLine("Objects are the same");
+            else
+                Console.WriteLine("Objects are not the same");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+
+    //
+    // Only one instance of object can be created.
+    // Returns created instance, if exists.
+    // For e.g. database connection object
+    //
+
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        public static Singleton Instance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+
+        protected Singleton()
+        {
+
+        }
+    }
+}
