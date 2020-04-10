6fa45861fd34f67c2a190594e35cf096b5febcbc
PanKunik <kunik12254@o2.pl>
2020-04-10 11:03:19 +0200

    Adding Factory Design Pattern

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        public static void doStuffEnemy(EnemyShip anEnemyShip)
+        {
+            anEnemyShip.displayEnemyShip();
+            anEnemyShip.followHeroShip();
+            anEnemyShip.enemyShipShoots();
+        }
+
+        static void Main(string[] args)
+        {
+            EnemyShip theEnemy;
+            EnemyShipFactory shipFactory = new EnemyShipFactory();
+
+            Console.Write("Which type of ship (U / R / B)?: ");
+            string enemyShipOption = Console.ReadLine();
+
+            theEnemy = shipFactory.makeEnemyShip(enemyShipOption);
+
+            doStuffEnemy(theEnemy);
+
+            Console.ReadKey();
+        }
+    }
+}
