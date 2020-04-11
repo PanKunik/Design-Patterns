af14c608300d377ca6431a019a3613a5977417a3
PanKunik <kunik12254@o2.pl>
2020-04-11 17:13:23 +0200

    Adding comments to Command design pattern

--- CommandDesignPattern/Program.cs
+++ CommandDesignPattern/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace CommandDesignPattern
 {
+    /// <summary>
+    /// With Command pattern you can set commands and execute them later (with execute() method).
+    /// You create class (Reciver) which will receive command from other class (Invoker 0 with execute() method) 
+    /// Commands can be executed multiple times
+    /// 
+    /// 1. Create device that will receive command (with TVRemote class)
+    /// 2. Create command class (for e.g. TvTurnOn)
+    /// 3. Create Invoker class (DeviceButton - pressing turn on button on TV remote)
+    /// 
+    /// When you press "Switch On" button on tv remote you are invoking command to turn on the television
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
